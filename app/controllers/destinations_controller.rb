765d8bb3ee6124816887a6744f394e4d98d76c1d
wgeorge1990 <williameverettgeorge@icloud.com>
2018-11-29 17:28:55 -0500

    another commit

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,31 @@
 class DestinationsController < ApplicationController
+
+  def index
+    @destinations = Destination.all
+  end
+
+  def show
+    @destination = Destination.find(params[:id])
+  end
+
+  def new
+    @destination = Destination.new
+  end
+
+  def create
+    @destination = Destination.new(destination_params)
+    if @destination.save
+      redirect_to destination_path(@destination)
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+    def destination_params
+      params.permit(:name, :country)
+    end
+
 end
